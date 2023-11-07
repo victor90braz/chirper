f24cc6a27176297c54b09b06b3600877e40b2c2c
victor braz <victor.90braz@gmail.com>
2023-11-07 16:10:21 +0100

    method view to render

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
-    public function index(): Response
+    public function index(): View
     {
-        return response('chirps.index');
+        return view('chirps.index');
     }
-
 
 }
