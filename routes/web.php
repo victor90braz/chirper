9a5b89b2b2d34d4dbe0b37d0d439a16edd9c18f0
victor braz <victor.90braz@gmail.com>
2023-11-07 10:00:04 +0100

    display user name from database

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $user = \App\Models\User::first();
+
+    return view('welcome', [
+        'user' => $user
+    ]);
 });
