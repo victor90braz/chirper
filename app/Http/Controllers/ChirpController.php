1967e5b727078bdd7226d6853f3bd4377b69868e
victor braz <victor.90braz@gmail.com>
2023-11-07 17:55:42 +0100

    edit and update

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -27,13 +27,25 @@ class ChirpController extends Controller
         return redirect(route('chirps.index'));
     }
 
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
+        $this->authorize('update', $chirp);
 
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
     }
 
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
+        $this->authorize('update', $chirp);
 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 }
